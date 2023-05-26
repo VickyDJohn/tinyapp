efd32cbc7c3fccd1b00a307f8ed383b046ab7a14
Vicky John <vicky.d.john@email.com>
2023-05-26 16:24:39 -0700

    added conditional to display error if URL is not found

--- express_server.js
+++ express_server.js
@@ -58,7 +58,11 @@ app.post('/urls', (req, res) => {
 app.get("/u/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("URL not found");
+  }
 });
 
 app.listen(PORT, () => {
