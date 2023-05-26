0205ab53cb730d4b55016e94c981131134b0c410
Vicky John <vicky.d.john@email.com>
2023-05-26 14:55:46 -0700

    added page /urls/new

--- express_server.js
+++ express_server.js
@@ -26,9 +26,13 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
-  console.log(req.params.id)
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  console.log(req.params.id);
   res.render('urls_show', templateVars);
 });
 
