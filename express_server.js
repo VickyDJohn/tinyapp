a2be11d6e7a82b3176e079f1d1195583268e2f7c
Vicky John <vicky.d.john@email.com>
2023-05-30 15:30:47 -0700

    fixed route to redirect to longURL

--- express_server.js
+++ express_server.js
@@ -61,9 +61,14 @@ app.get('/urls/new', (req, res) => {
 
 // Route: Show details of a specific URL
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    username: req.cookies['username'],
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
   res.render('urls_show', templateVars);
 });
+
 
 // Route: Create a new URL
 app.post('/urls', (req, res) => {
