d1e830a3ff249733ddd8c29342c67bd9786b7023
Vicky John <vicky.d.john@email.com>
2023-05-31 12:28:58 -0700

    changed cookie value from username to user_id and changed POST routes to enable site to work for various scenarios

--- express_server.js
+++ express_server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 
 app.use(cookieParser());
+app.use(morgan('dev'));
 
-const users = {};
+const users = {
+
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -47,16 +51,19 @@ app.get('/hello', (req, res) => {
 // Route: Display all URLs
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies['username'],
+    user_id: req.cookies['user_id'],
     urls: urlDatabase,
+    users
   };
   res.render("urls_index", templateVars);
+  console.log(users);
 });
 
 // Route: Form to create a new URL
 app.get('/urls/new', (req, res) => {
   const templateVars = {
-    username: req.cookies['username'],
+    user_id: req.cookies['user_id'],
+    users
   };
   res.render('urls_new', templateVars);
 });
@@ -64,15 +71,14 @@ app.get('/urls/new', (req, res) => {
 // Route: Show details of a specific URL
 app.get('/urls/:id', (req, res) => {
   const templateVars = {
-    username: req.cookies['username'],
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    id: req.params.id, longURL: urlDatabase[req.params.id],
+    user_id: req.cookies['user_id'],
+    users
   };
   res.render('urls_show', templateVars);
 });
 
-
-// Route: Create a new URL
+// POST: Create a new URL
 app.post('/urls', (req, res) => {
   const id = generateRandomString();
   const longURL = req.body.longURL;
@@ -92,44 +98,16 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-// Route: Registration page
-app.get('/register', (req, res) => {
+//Route: Register page
+app.get("/register", (req, res) => {
   const templateVars = {
-    username: req.cookies['username'],
-    urls: urlDatabase,
+    user_id: req.cookies['user_id'],
+    urls: urlDatabase
   };
-  res.render('register', templateVars);
+  res.render('register');
 });
 
-// Route: Delete a URL
-app.post('/urls/:id/delete', (req, res) => {
-  const id = req.params.id;
-  delete urlDatabase[id];
-  res.redirect('/urls');
-});
-
-// Route: Update a URL
-app.post('/urls/:id', (req, res) => {
-  const id = req.params.id;
-  const newLongURL = req.body.longURL;
-  urlDatabase[id] = newLongURL;
-  res.redirect('/urls');
-});
-
-//Route: login and set username cookie
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  res.cookie('username', username);
-  res.redirect('/urls');
-});
-
-// Route: Logout
-app.post('/logout', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/urls');
-});
-
-// POST: User Registration
+//POST: Save new user registration details
 app.post('/register', (req, res) => {
   const id = generateRandomString();
   const { email, password } = req.body;
@@ -142,6 +120,39 @@ app.post('/register', (req, res) => {
 
   users[id] = newUser;
   res.cookie('user_id', id);
+  res.redirect('/urls');
+  console.log(users);
+});
+
+// POST: Delete a URL
+app.post('/urls/:id/delete', (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect('/urls');
+});
+
+// POST: Update a URL
+app.post('/urls/:id', (req, res) => {
+  const id = req.params.id;
+  const newLongURL = req.body.longURL;
+  urlDatabase[id] = newLongURL;
+  res.redirect('/urls');
+});
+
+//POST: login and set user_id cookie
+app.post('/login', (req, res) => {
+  const user = Object.values(users).find(user => user.email === user_id);
+  if (user) {
+    res.cookie('user_id', user_id);
+    res.redirect('/urls');
+  } else {
+    res.status(401).send('User not found. Please <a href="/register">register</a>');
+  }
+});
+
+// POST: Logout
+app.post('/logout', (req, res) => {
+  res.clearCookie('user_id');
   res.redirect('/urls');
   console.log(users);
 });
